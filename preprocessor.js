82ef0060f9b515aac703a29278444d3e0629bb36
Dan Vanderkam <danvdk@gmail.com>
2014-09-26 12:53:19 -0400

    Passing test based on React/Jest tutorial

---
+++ preprocessor.js
@@ -0,0 +1,9 @@
+// JSX preprocessor for Jest tests
+// See http://facebook.github.io/jest/docs/tutorial-react.html
+
+var ReactTools = require('react-tools');
+module.exports = {
+  process: function(src) {
+    return ReactTools.transform(src, {harmony: true});
+  }
+};
